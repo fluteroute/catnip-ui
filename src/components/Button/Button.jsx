a5d3f93bec4dbeccdf94d1c4db41a4b48623a325
Ashley David <ashley.nichole@hotmail.com>
2022-12-08 19:53:39 -0500

    Set up storybook

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// export interface ButtonProps
+//   extends React.Component<typeof HTMLButtonElement> {}
+
+function Button(props) {
+  const { children, ...other } = props;
+  return (
+    <button type="button" {...other}>
+      {children}
+    </button>
+  );
+}
+
+Button.displayName = 'CatNipButton'
+
+export default Button;
---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../components/Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: Button,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args}>Button</Button>;
+
+export const Primary = Template.bind({});
