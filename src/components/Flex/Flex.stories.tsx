aef597cb6fbbe4978d64e956603cdb7b787a1f8f
fluteroute <ashley@ashleyndavid.com>
2023-03-05 16:18:02 -0500

    Added Flex

---
+++ src/components/Flex/Flex.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Box } from '../Box';
+import { Flex } from '.';
+import { Typography } from '../Typography';
+
+export default {
+  title: 'Example/Flex',
+  component: Flex,
+} as ComponentMeta<typeof Flex>;
+
+const Template: ComponentStory<typeof Flex> = (args) => (
+  <Flex {...args}>
+    <Box p={2} bg="primary" color="white" sx={{ flex: '1 1 auto' }}>
+      <Typography variant="body1">Flex</Typography>
+    </Box>
+    <Box p={2} bg="secondary">
+      <Typography variant="body1">Box</Typography>
+    </Box>
+  </Flex>
+);
+
+export const Default = Template.bind({});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,20 @@
+export * from './Box';
+
 export * from './Button';
 
+export * from './Card';
+
 export * from './CatnipProvider';
+
+export * from './Divider';
+
+export * from './Flex';
+
+export * from './Grid';
+
+export * from './Image';
+
+export * from './TextArea';
 
 export * from './TextField';
 
